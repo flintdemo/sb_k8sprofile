49cfc7b66fb77ba335e7aa5ef79bee9cb6c0ba25
flintdemo <82654153+flintdemo@users.noreply.github.com>
2021-06-03 11:28:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "global" {
   backend = "remote"
   config = {
-    organization = "CiscoDevNet"
+    organization = "Flint"
     workspaces = {
       name = var.globalwsname
     }
